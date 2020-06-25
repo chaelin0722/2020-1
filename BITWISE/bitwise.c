e7658051be397825fc20bab9c9ec84ea3707d18f
alicia <53431568+chaelin0722@users.noreply.github.com>
2020-06-25 12:59:53 +0900

    bitwise

---
+++ BITWISE/bitwise.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+union sh{
+	int i;
+	unsigned char c[4];
+};
+
+int main(void){
+	int i,j;
+	union sh p,q;
+	i = -1121;
+	j = i>>3;
+	
+	p.i = i;
+	q.i = j;
+	
+	printf("%d, %X, %X, %X, %X\n",p.i, p.c[3], p.c[2], p.c[1], p.c[0]);
+	printf("%d, %X, %X, %X, %X\n",q.i, q.c[3], q.c[2], q.c[1], q.c[0]);
+	return 0;
+}
---
+++ BITWISE/sw.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(void){
+	int i = 0xABCDEF98;
+	
+	return 0;
+}
