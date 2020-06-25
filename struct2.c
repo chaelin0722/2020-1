6bbf4703e2511641448f36bed538e188d5962013
alicia <53431568+chaelin0722@users.noreply.github.com>
2020-06-25 10:52:50 +0900

    struct typedef ver.

--- struct2.c
+++ struct2.c
@@ -3,16 +3,20 @@
 #include<string.h>
 
 int main(void){
+	typedef int PRICE; //PRICE 라는 타입이 만들어짐 
 	
-	struct itemInfo{
+		
+	typedef struct itemInfo{
 		char name[16];
-		int price;	
-	};
+		PRICE price;	
+	} INFO;
 	
-	struct itemInfo	item1 = {"computer", 1600}, item2;
+	struct itemInfo	item1 = {"computer", 1600}; 
+	//아까 버전과 똑같다. 선언방식의 차이일뿐 
 	
-	item2 = item1;
+	INFO item2;
 	
+	item2 = item1;  
 	printf("%s, %d\n", item2.name, item2.price);
 
 	return 0;
